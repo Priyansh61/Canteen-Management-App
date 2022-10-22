691e20d2525f01470da9266aa7981998387d3864
Priyansh61 <21bec080@iiitdmj.ac.in>
2022-10-22 19:18:00 +0530

    product routing added

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app=express();
 const userRoute=require('./routes/user');
 
 const categoryRoute=require('./routes/category');
+const productRoute=require('./routes/product');
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/user',userRoute);
 app.use('/category',categoryRoute);
+app.use("/product",productRoute);
 
 module.exports=app;
--- table.sql
+++ table.sql
@@ -13,7 +13,31 @@ create table user(
 insert into user(name,password,contactNo,email,status,role) values ('admin','123',1234567890','admin@gmail.com','active','admin');
 
 
-create table category {
+create table category (
     id int primary key auto_increment,
     name varchar(100) NOT NULL,
-};
+);
+
+create table product (
+    id int primary key auto_increment,
+    name varchar(100) NOT NULL,
+    description varchar(100) NOT NULL,
+    price int NOT NULL,
+    category_id int NOT NULL,
+    status int NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+create table bill (
+    id int primary key auto_increment,
+    uuid varchar(200) NOT NULL,
+    name varchar(100) NOT NULL,
+    contactNo varchar(10) NOT NULL,
+    email varchar(100) NOT NULL,
+    paymentMethod varchar(100) NOT NULL,
+    total int NOT NULL,
+    productDetails JSON Default NULL,
+    CreatedBy varchar(100) NOT NULL,
+    CreatedDate datetime NOT NULL
+);
+
