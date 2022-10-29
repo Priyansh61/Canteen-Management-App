0b837bda24b564f965d6a592c921170468d9fd37
Priyansh61 <21bec080@iiitdmj.ac.in>
2022-10-30 00:10:20 +0530

    token intercep added to change header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,10 +13,11 @@ import { SharedModule } from './shared/shared.module';
 import { FullComponent } from './layouts/full/full.component';
 import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SignupComponent } from './signup/signup.component';
 import {NgxUiLoaderModule,NgxUiLoaderConfig,SPINNER,PB_DIRECTION} from 'ngx-ui-loader';
 import { LoginComponent } from './login/login.component';
+import { TokenIntercepInterceptor } from './services/token-intercep.interceptor';
 
 const ngxuiLoaderConfig:NgxUiLoaderConfig = {
   text: 'Loading...',
@@ -53,7 +54,7 @@ const ngxuiLoaderConfig:NgxUiLoaderConfig = {
     HttpClientModule,
     NgxUiLoaderModule.forRoot(ngxuiLoaderConfig)
   ],
-  providers: [],
+  providers: [HttpClientModule,{provide:HTTP_INTERCEPTORS,useClass:TokenIntercepInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
